a2a1b64afa477c4e07e4c1d0d4b5383c5e04d810
joy-lj <joyluya@gmail.com>
2024-11-03 20:56:38 -0500

    fixing contact form

--- library-site/src/components/ContactForm.js
+++ library-site/src/components/ContactForm.js
@@ -24,7 +24,7 @@ const ContactForm = () => {
                         required
                     />
                 </p>
-                <p>
+                <p id="email-input">
                     <input id="email" type="email" placeholder="Email" name="email" required />
                 </p>
                 <p>
